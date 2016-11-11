7bedaf24aa54738cf2c152e223dbe906257d1fca
atrp <alex@greenbit.ro>
2016-11-11 20:56:33 +0200

    commit

--- app.js
+++ app.js
@@ -1,7 +1,2 @@
-var express = require("express");
-var path = require("path");
-var bodyParser = require("body-parser");
-
-var app = express();
-app.use(express.static(__dirname + "/public"));
-app.use(bodyParser.json());
+// Call the console.log function.
+console.log("Hello World");
