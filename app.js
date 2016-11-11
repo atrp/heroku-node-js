a42f8ebf2ea706c8699f055ffceda723ed11338a
atrp <alex@greenbit.ro>
2016-11-11 21:11:29 +0200

    commit

--- app.js
+++ app.js
@@ -18,6 +18,13 @@ app.get('/', function(req, res) {
 	res.render('index');
 });
 
+// set the contact page route
+app.get('/contact/', function(req, res) {
+
+        // ejs render automatically looks in the views folder
+        res.render('contact');
+});
+
 app.listen(port, function() {
 	console.log('Our app is running on http://localhost:' + port);
 });
