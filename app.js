4ea8eed5d1ea776048f9feca74c15b009cdfa851
atrp <alex@greenbit.ro>
2016-11-11 19:28:55 +0200

    1st web app

--- app.js
+++ app.js
@@ -1,14 +1,2 @@
-var http = require("http");
-
-http.createServer(function (request, response) {
-   // Send the HTTP header 
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   
-   // Send the response body as "Hello World"
-   response.end('Hello World\n');
-}).listen(443);
-
 // Console will print the message
 console.log('Server running at https://node-js-ro.herokuapp.com/');
